57f42c20ed5c1e5883b3c106dc12a612c21fa578
Reinhard Vicinus <r.vicinus@metaways.de>
2014-11-10 20:36:33 +0100

    added logrotate with multiple paths

---
+++ manifests/rulepath.pp
@@ -0,0 +1,23 @@
+define logrotate::rulepath (
+  $rulebody = undef,
+  $order = "01",
+  $hourly = false,
+  $path = undef,
+) {
+
+  if (is_array($path)) {
+    $rpath = join($path, " ")
+  } else {
+    $rpath = $path
+  }
+  if ($hourly) {
+    $rule_path = "/etc/logrotate.d/hourly/${rulebody}"
+  } else {
+    $rule_path = "/etc/logrotate.d/${rulebody}"
+  }
+  concat::fragment { $name:
+    target => $rule_path,
+    order => $order,
+    content => $rpath,
+  }
+}
